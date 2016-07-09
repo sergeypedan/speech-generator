816e5ad2e8f1f0afdab40c539a8594c8d899bd8d
Sergey Pedan <sergey.pedan@gmail.com>
2016-07-09 13:51:14 +0500

    Converts TAB to 2 spaces

--- speaker.rb
+++ speaker.rb
@@ -1,27 +1,27 @@
 class Speaker
 
-	def initialize(speach_components)
-		@speach_components = speach_components
-	end
+  def initialize(speach_components)
+    @speach_components = speach_components
+  end
 
-	def say_sentence
-		sentence(@speach_components)
-	end
+  def say_sentence
+    sentence(@speach_components)
+  end
 
-	private
+  private
 
-	def sentence(speach_components)
-		components = ( 1..speach_components.count ).map { |i| speach_components[ i - 1] }
-		parts = components.map { |component_variants| random_from component_variants }
-		parts.join(" ") + "."
-	end
+  def sentence(speach_components)
+    components = ( 1..speach_components.count ).map { |i| speach_components[ i - 1] }
+    parts = components.map { |component_variants| random_from component_variants }
+    parts.join(" ") + "."
+  end
 
-	def random_from(array)
-		raise ArgumentError.new "Nil passed to `random_from`"         if array == nil
-		raise ArgumentError.new "Empty array passed to `random_from`" if array == []
+  def random_from(array)
+    raise ArgumentError.new "Nil passed to `random_from`"         if array == nil
+    raise ArgumentError.new "Empty array passed to `random_from`" if array == []
 
-		index = rand( array.count )
-		array[index]
-	end
+    index = rand( array.count )
+    array[index]
+  end
 
 end
